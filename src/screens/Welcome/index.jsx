139fa38d651c107525f211ac111604ec4bce3980
Joanderson Batista <joandersonbatista.br@gmail.com>
2021-09-01 17:22:07 -0300

    login

--- src/screens/Welcome/index.jsx
+++ src/screens/Welcome/index.jsx
@@ -1,12 +1,91 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
+// eslint-disable-next-line camelcase
+import { useFonts, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import {
-  BacgroundImage,
+  Svg, Defs, RadialGradient, Stop, Ellipse,
+} from 'react-native-svg';
+import {
+  WelcomeImage,
+  ContainerLogin,
+  LoginFaceButton,
+  ButtonContent,
+  LoginFacebookText,
+  LoginEmailButton,
+  LoginEmailText,
+  SignUpContainer,
+  SignUpText,
+  SignUpTextSpan,
+  LogoContainer,
+  height,
 } from './styled';
 
+import WelcomeLogo from '../../assets/welcome.jpeg';
+import Logo from '../../assets/Logo.svg';
+import LogoFace from '../../assets/Vector.svg';
+
 export default function Login() {
+  const [fontsLoaded] = useFonts({
+    Roboto_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <BacgroundImage />
+      <StatusBar hidden />
+      <WelcomeImage source={WelcomeLogo} resizeMode="cover">
+        <Svg height="100%" width="100%">
+          <Defs>
+            <RadialGradient
+              id="grad"
+              cx="50%"
+              cy="50%"
+              rx="230"
+              ry="230"
+              gradientUnits="userSpaceOnUse"
+            >
+              <Stop offset="0" stopColor="#2626284D" stopOpacity="0.3" />
+              <Stop offset="1" stopColor="#262628CC" stopOpacity="0.8" />
+            </RadialGradient>
+          </Defs>
+          <Ellipse cx="150" cy="75" rx={height * 0.57} ry="100%" fill="url(#grad)" />
+        </Svg>
+      </WelcomeImage>
+      <ContainerLogin>
+        <LoginFaceButton>
+          <ButtonContent>
+            <LogoFace height={height * 0.036} />
+            <LoginFacebookText
+              fontFamily="Roboto_500Medium"
+            >
+              Entre com Facebook
+            </LoginFacebookText>
+          </ButtonContent>
+        </LoginFaceButton>
+        <LoginEmailButton>
+          {/* eslint-disable-next-line camelcase */}
+          <LoginEmailText
+            fontFamily="Roboto_500Medium"
+          >
+            Entre com seu e-mail
+          </LoginEmailText>
+        </LoginEmailButton>
+        <SignUpContainer>
+          <SignUpText
+            fontFamily="Roboto_500Medium"
+          >
+            Not a menber?
+            <SignUpTextSpan> Signup now</SignUpTextSpan>
+          </SignUpText>
+        </SignUpContainer>
+        <LogoContainer>
+          <Logo height={height * 0.05} />
+        </LogoContainer>
+      </ContainerLogin>
     </>
   );
 }
--- src/screens/Welcome/styled.jsx
+++ src/screens/Welcome/styled.jsx
@@ -1,12 +1,88 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+
+export const { height, width } = Dimensions.get('window');
 
 export const BacgroundImage = styled.View`
-  height: 50px;
+  height: 57%;
   width: 100%;
-  background-color: blue;
+  background-color: rgb(0, 0, 255);
+  position: absolute;
 `;
 
-export const BacgroundImage2 = styled.View`
+export const WelcomeImage = styled.ImageBackground`
+  height: 57%;
+  width: 100%;
+  position: absolute;
+`;
+
+export const ContainerLogin = styled.View`
+  height: 46%;
+  width: 100%;
+  position: absolute;
+  background-color: white;
+  bottom: 0;
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+  align-items: center;
+`;
+
+export const LoginFaceButton = styled.TouchableOpacity`
+  width: 81%;
+  height: ${height * 0.08}px;
+  background-color: #3B5999;
+  margin-top: ${height * 0.06}px;
+  border-radius: 14px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const LoginFacebookText = styled.Text`
+  font-size: 14px;
+  color: white;
+  font-family: ${(props) => props.fontFamily};
+  line-height: 16px;
+  padding-left: 20px;
+`;
+
+export const LoginEmailButton = styled.TouchableOpacity`
+  width: 81%;
+  height: ${height * 0.08}px;
+  background-color: #262628;
+  border-radius: 14px;
+  margin-top: 15px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoginEmailText = styled.Text`
+  font-size: 14px;
+  color: white;
+  font-family: ${(props) => props.fontFamily};
+  line-height: 16px;
+`;
+
+export const SignUpContainer = styled.TouchableOpacity`
+  margin-top: 50px;
+`;
+
+export const SignUpText = styled.Text`
+  font-size: 14px;
+  color: black;
+  font-family: ${(props) => props.fontFamily};
+  line-height: 16px;
+`;
+
+export const SignUpTextSpan = styled.Text`
+  color: #43A2FA;
+`;
+
+export const LogoContainer = styled.View`
   flex: 1;
-  background-color: blue;
+  padding-top: 14px;
 `;
