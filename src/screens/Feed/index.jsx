beb6309ed5b560e5df7593fc209ec2f874e7afc8
joandersonbatista <joandersonbatista.br@gmail.com>
2021-09-13 17:01:29 -0300

    feed

--- src/screens/Feed/index.jsx
+++ src/screens/Feed/index.jsx
@@ -1,41 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
-import { G, Circle, Svg } from 'react-native-svg';
+import { StatusBar } from 'expo-status-bar';
+import {
+  Hp,
+  Header,
+  InstagramText,
+  ContainerIcons,
+  LikeIcon,
+  MessageIcon,
+  SearchIcon,
+} from './styled';
+
+import Logo from '../../assets/Logo.svg';
 
 export default function Feed() {
-  const sera = 2 * Math.PI * 45;
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: 'black',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-    >
-      <Svg height="100" width="100">
-        <G rotation={-90} origin={[50, 50]}>
-          <Circle
-            cx="50"
-            cy="50"
-            r="45"
-            fill="transparent"
-            strokeWidth="5"
-            stroke="pink"
-            strokeOpacity={0.2}
-          />
-          <Circle
-            cx="50"
-            cy="50"
-            r="45"
-            fill="transparent"
-            strokeWidth="5"
-            stroke="blue"
-            strokeDasharray={sera}
-            strokeDashoffset={sera * 0.50}
-            strokeLinecap="round"
-          />
-        </G>
-      </Svg>
-    </View>
+    <>
+      <StatusBar backgroundColor="white" style="dark-content" />
+      <Header>
+        <Logo height={Hp(40)} />
+        <InstagramText>
+          Instagram
+        </InstagramText>
+        <ContainerIcons>
+          <MessageIcon />
+          <LikeIcon />
+          <SearchIcon />
+        </ContainerIcons>
+      </Header>
+    </>
   );
 }
